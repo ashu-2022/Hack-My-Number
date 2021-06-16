a16b0193e2c864da3b7d48bb531d46bb5e75060c
Ashu <ashutoshpandit308@gmail.com>
2021-06-16 12:19:04 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,57 @@
+
+'use strict';
+
+// generate secret numbers
+let secretNumber=Math.trunc(Math.random()*20)+1;
+let score = 20;
+let highscore = 0;
+
+// function to display message
+const displayMessage = function(message){
+    document.querySelector('.message').textContent = message;
+};
+
+// function to validate user no with guess no
+document.querySelector('.check').addEventListener('click',function(){
+    const guess = Number(document.querySelector('.guess').value);
+
+    if(!guess){
+        displayMessage("⛔️No Number!");
+    }
+    else if (guess===secretNumber){
+        displayMessage('🎉 Correct Number!');
+        document.querySelector('.number').textContent = secretNumber;
+        document.body.style.background="#60b347";
+        document.querySelector('.number').style.width = '30rem';
+        
+        //update highscore
+        if(score>highscore){
+            highscore=score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+    }else if(guess!==secretNumber){
+        if (score>1){
+            displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+            score--;
+            document.querySelector('.score').textContent = score;
+        }else{
+            displayMessage("💥 You lost the game!");
+            document.querySelector('body').style.backgroundColor = 'red';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+});
+
+// function to set new Game 
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    displayMessage('Start guessing...');
+
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+  });
+
